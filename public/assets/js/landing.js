effbb271be8cae9b43366282730c99ff2fcfce7e
Adam Łożyński <lozynskiadam@gmail.com>
2021-01-11 20:21:35 +0100

    landing page

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -1,7 +1,7 @@
 (function ($) {
     let app = {};
     app.header = function () {
-        let $header = $('.header-main');
+        let $header = $('.header-main', document);
         if ($(window).scrollTop() === 0 && $(window).width() >= 975) {
             $header.addClass("navbar-dark");
             $header.removeClass("navbar-light");
@@ -9,6 +9,17 @@
             $header.addClass("navbar-light");
             $header.removeClass("navbar-dark");
         }
+        document.querySelectorAll(".nav-link").forEach(link => {
+            let section = document.querySelector(link.hash);
+            if (
+                section.offsetTop <= window.scrollY &&
+                section.offsetTop + section.offsetHeight > window.scrollY
+            ) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        });
     };
     app.parallax = function () {
         $('.jarallax').jarallax({
@@ -18,6 +29,17 @@
     $(document).ready(function () {
         app.header();
         app.parallax();
+        $("a").on('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                let hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 600, function(){
+                    window.location.hash = hash;
+                });
+            }
+        });
     });
     $(window).scroll(function () {
         app.header();
@@ -25,4 +47,5 @@
     $(window).resize(function () {
         app.header();
     });
+
 })(jQuery);
