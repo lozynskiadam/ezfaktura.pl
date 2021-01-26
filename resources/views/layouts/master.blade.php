7def6fdeb658a4f41b5da1a0b33d05e4c726e7b7
Adam Łożyński <lozynskiadam@gmail.com>
2021-01-26 20:25:12 +0100

    blade asset()

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,14 +5,14 @@
   <title>{{ env('APP_NAME') }}</title>
   <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport'/>
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="icon" href="/assets/img/icon.png" type="image/png"/>
+  <link rel="icon" href="{{ asset('/assets/img/icon.png') }}" type="image/png"/>
 
-  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/assets/css/atlantis.css">
-  <link rel="stylesheet" href="/assets/css/ezfaktura.css">
+  <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/assets/css/atlantis.css') }}">
+  <link rel="stylesheet" href="{{ asset('/assets/css/ezfaktura.css') }}">
 
   <!-- Fonts and icons -->
-  <script src="/assets/js/plugin/webfont/webfont.min.js"></script>
+  <script src="{{ asset('/assets/js/plugin/webfont/webfont.min.js') }}"></script>
   <script>
     WebFont.load({
       google: {"families": ["Lato:300,400,700,900", "Khand:300,400,500,600,700"]},
@@ -65,52 +65,49 @@
 </div>
 
 <!-- Core JS Files -->
-<script src="/assets/js/core/jquery.3.2.1.min.js"></script>
-<script src="/assets/js/core/popper.min.js"></script>
-<script src="/assets/js/core/bootstrap.min.js"></script>
+<script src="{{ asset('/assets/js/core/jquery.3.2.1.min.js') }}"></script>
+<script src="{{ asset('/assets/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('/assets/js/core/bootstrap.min.js') }}"></script>
 
 <!-- jQuery UI -->
-<script src="/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-<script src="/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+<script src="{{ asset('/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js') }}"></script>
 
 <!-- jQuery Scrollbar -->
-<script src="/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+<script src="{{ asset('/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
 
 <!-- Chart JS -->
-<script src="/assets/js/plugin/chart.js/chart.min.js"></script>
+<script src="{{ asset('/assets/js/plugin/chart.js/chart.min.js') }}"></script>
 
 <!-- jQuery Sparkline -->
-<script src="/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
-
-<!-- Chart Circle -->
-<script src="/assets/js/plugin/chart-circle/circles.min.js"></script>
+<script src="{{ asset('/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
 
 <!-- Datepicker -->
-<script src="/assets/js/plugin/datepicker/bootstrap-datepicker.min.js"></script>
-<script src="/assets/js/plugin/datepicker/locales/pl.min.js"></script>
-<link rel="stylesheet" href="/assets/js/plugin/datepicker/bootstrap-datepicker3.min.css"/>
+<script src="{{ asset('/assets/js/plugin/datepicker/bootstrap-datepicker.min.js') }}"></script>
+<script src="{{ asset('/assets/js/plugin/datepicker/locales/pl.min.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('/assets/js/plugin/datepicker/bootstrap-datepicker3.min.css') }}"/>
 
 <!-- Datatables -->
-<script src="/assets/js/plugin/datatables/datatables.min.js"></script>
-<script src="/assets/js/plugin/datatables/dataTables.buttons.min.js"></script>
-<link rel="stylesheet" href="/assets/js/plugin/datatables/buttons.dataTables.min.css">
-<script src="/assets/js/renderers.js"></script>
+<script src="{{ asset('/assets/js/plugin/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('/assets/js/plugin/datatables/dataTables.buttons.min.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('/assets/js/plugin/datatables/buttons.dataTables.min.css') }}">
+<script src="{{ asset('/assets/js/renderers.js') }}"></script>
 
 <!-- Bootstrap Notify -->
-<script src="/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+<script src="{{ asset('/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
 
 <!-- jQuery Vector Maps -->
-<script src="/assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
-<script src="/assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
+<script src="{{ asset('/assets/js/plugin/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ asset('/assets/js/plugin/jqvmap/maps/jquery.vmap.world.js') }}"></script>
 
 <!-- Sweet Alert -->
-<script src="/assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+<script src="{{ asset('/assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
 
 <!-- Atlantis JS -->
-<script src="/assets/js/atlantis.js"></script>
+<script src="{{ asset('/assets/js/atlantis.js') }}"></script>
 
 <!-- Dialog -->
-<script src="/assets/js/plugin/dialog/dialog.js"></script>
+<script src="{{ asset('/assets/js/plugin/dialog/dialog.js') }}"></script>
 
 @yield('scripts')
 
